edace26664b03265a2ba2f021fe6ba9b790d3d3b
victormerch <1Victormerch@gmail.com>
2021-11-09 16:00:07 +0100

    librerias RMI Lipe

--- app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
+++ app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
@@ -1,6 +1,8 @@
 package com.example.kadamm.ui.connexio;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.kadamm.MainActivity;
 import com.example.kadamm.databinding.FragmentConnexioBinding;
+
+import java.io.IOException;
+
+import lipermi.handler.CallHandler;
+import lipermi.net.Client;
 
 
 public class ConnexioFragment extends Fragment {
@@ -22,6 +30,7 @@ public class ConnexioFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         connexioViewModel =
                 new ViewModelProvider(this).get(ConnexioViewModel.class);
 
@@ -42,5 +51,27 @@ public class ConnexioFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    class Conn extends AsyncTask<Void, Void, MainActivity> {
+
+
+        protected MainActivity doInBackground(String ip) {
+            Looper.prepare();
+            try {
+                CallHandler callHandler = new CallHandler();
+                String serverIP;
+                Client client = new Client(ip, 7777, callHandler);
+               // TestService testService = (TestService) client.getGlobal(TestService.class);
+
+
+                System.out.println("Se ha conectado correctamente");
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Looper.loop();
+            return null;
+        }
+
     }
 }
