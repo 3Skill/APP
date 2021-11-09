b2a90eadbf3c111c526fb109ea27e1615c0983d3
victormerch <1Victormerch@gmail.com>
2021-11-09 17:16:51 +0100

    Integracion del cliente RMILipe, falta pillar el edittext y cambiar el semaforo

--- app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
+++ app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
@@ -4,18 +4,24 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kadamm.MainActivity;
+import com.example.kadamm.R;
 import com.example.kadamm.databinding.FragmentConnexioBinding;
+
 
 import java.io.IOException;
 
@@ -25,17 +31,28 @@ import lipermi.net.Client;
 
 public class ConnexioFragment extends Fragment {
 
+
     private ConnexioViewModel connexioViewModel;
     private FragmentConnexioBinding binding;
-
+    private String serverIP = null;
+    //private String serverIP = "192.168.0.176";
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        connexioViewModel =
-                new ViewModelProvider(this).get(ConnexioViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_connexio, container, false);
+        Button button = (Button) v.findViewById(R.id.btnConnecta);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        binding = FragmentConnexioBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+                EditText et1 = (EditText) v.findViewById(R.id.ipUsuari);
+                String texto = et1.getText().toString();
+                //System.out.println(texto.isEmpty());
+                serverIP = texto;
+                new Conn().execute();
+            }
+        });
+
 
 //        final TextView textView = binding.textHome;
 //        connexioViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -44,7 +61,7 @@ public class ConnexioFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
-        return root;
+        return v;
     }
 
     @Override
@@ -55,19 +72,19 @@ public class ConnexioFragment extends Fragment {
     class Conn extends AsyncTask<Void, Void, MainActivity> {
 
 
-        protected MainActivity doInBackground(String ip) {
+        protected MainActivity doInBackground(Void... params) {
             Looper.prepare();
             try {
                 CallHandler callHandler = new CallHandler();
-                String serverIP;
-                Client client = new Client(ip, 7777, callHandler);
-               // TestService testService = (TestService) client.getGlobal(TestService.class);
 
+                Client client = new Client(serverIP, 1110, callHandler);
+               // TestService testService = (TestService) client.getGlobal(TestService.class);
+                Toast.makeText(getContext(), "Servidor disponible", Toast.LENGTH_SHORT).show();
 
                 System.out.println("Se ha conectado correctamente");
                 client.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("No se ha conectado correctamente");
             }
             Looper.loop();
             return null;
