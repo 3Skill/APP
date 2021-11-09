b4b47f27eb06e1c02847056ba3cd5b7caa20f42c
victormerch <1Victormerch@gmail.com>
2021-11-09 18:49:19 +0100

    ya funciona el coger el editext y poner el semaforo en amarillo

--- app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
+++ app/src/main/java/com/example/kadamm/ui/connexio/ConnexioFragment.java
@@ -2,6 +2,7 @@ package com.example.kadamm.ui.connexio;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -34,33 +36,25 @@ public class ConnexioFragment extends Fragment {
 
     private ConnexioViewModel connexioViewModel;
     private FragmentConnexioBinding binding;
-    private String serverIP = null;
-    //private String serverIP = "192.168.0.176";
+    private String serverIP = "";
+    private Handler handler;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        handler = new Handler(Looper.getMainLooper());
         View v = inflater.inflate(R.layout.fragment_connexio, container, false);
+
         Button button = (Button) v.findViewById(R.id.btnConnecta);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 EditText et1 = (EditText) v.findViewById(R.id.ipUsuari);
                 String texto = et1.getText().toString();
-                //System.out.println(texto.isEmpty());
+
                 serverIP = texto;
                 new Conn().execute();
             }
         });
 
-
-//        final TextView textView = binding.textHome;
-//        connexioViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return v;
     }
 
@@ -75,11 +69,19 @@ public class ConnexioFragment extends Fragment {
         protected MainActivity doInBackground(Void... params) {
             Looper.prepare();
             try {
+
                 CallHandler callHandler = new CallHandler();
 
                 Client client = new Client(serverIP, 1110, callHandler);
-               // TestService testService = (TestService) client.getGlobal(TestService.class);
+
                 Toast.makeText(getContext(), "Servidor disponible", Toast.LENGTH_SHORT).show();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView tuImageView = (ImageView) getView().findViewById(R.id.imgViewSemafor);
+                        tuImageView.setImageResource(R.drawable.traffic_light_orange);
+                    }
+                });
 
                 System.out.println("Se ha conectado correctamente");
                 client.close();
