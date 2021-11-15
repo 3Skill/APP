3c19e0d8046b5016398e0b153798b7b194ef5163
Jose <jrosillonieto@gmail.com>
2021-11-15 17:38:38 +0100

    Implementado el fragment con SharedPreferences del nickname

--- app/src/main/java/com/example/kadamm/ui/usuari/UsuariFragment.java
+++ app/src/main/java/com/example/kadamm/ui/usuari/UsuariFragment.java
@@ -1,10 +1,15 @@
 package com.example.kadamm.ui.usuari;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +17,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.kadamm.R;
 import com.example.kadamm.databinding.FragmentUsuariBinding;
 
 public class UsuariFragment extends Fragment {
 
     private UsuariViewModel usuariViewModel;
     private FragmentUsuariBinding binding;
+    private TextView nickNameTitle;
+    private EditText nickName;
+    private Button saveNickName;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+
+    private String text;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,14 +41,50 @@ public class UsuariFragment extends Fragment {
         binding = FragmentUsuariBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        usuariViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//        final TextView textView = binding.textDashboard;
+//        usuariViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        nickNameTitle = (TextView) root.findViewById(R.id.tvNickname);
+        nickName = (EditText) root.findViewById(R.id.etNickname);
+        saveNickName = (Button) root.findViewById(R.id.btnSaveNickname);
+
+        saveNickName.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                nickNameTitle.setText(nickName.getText());
+                saveData();
             }
         });
+
+        loadData();
+        updateViews();
+
         return root;
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, nickNameTitle.getText().toString());
+
+        editor.apply();
+
+        Toast.makeText(this.getActivity(), "Usuari Guardat", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+
+    }
+
+    public void updateViews() {
+        nickNameTitle.setText(text);
     }
 
     @Override
