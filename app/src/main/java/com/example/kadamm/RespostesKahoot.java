ac48f8ae6a7e48556bea14abbe5c9efa304f62d7
Jose <jrosillonieto@gmail.com>
2021-11-30 18:30:44 +0100

    Spec 29 finalizada

--- app/src/main/java/com/example/kadamm/RespostesKahoot.java
+++ app/src/main/java/com/example/kadamm/RespostesKahoot.java
@@ -13,6 +13,8 @@ import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class RespostesKahoot extends AppCompatActivity {
     private TextView countdown;
     private Button btn1;
@@ -20,28 +22,55 @@ public class RespostesKahoot extends AppCompatActivity {
     private Button btn3;
     private Button btn4;
     private int counter;
+    private int lengthAnswers;
+    private boolean isFirstQuestion;
+    private ArrayList<String> respostes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-        //Create Intent
-//        Intent intent = new Intent(MainActivity.this,RespostesKahoot.class);
-//        intent.putExtra("extra_text", string);
-//        startActivity(intent);
-
-        // Get intent
-//        String text = getIntent().getStringExtra("extra_text");
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.respostes_kahoot);
 
-        countdown = findViewById(R.id.tvCountdown);
-        counter = Integer.parseInt(countdown.getText().toString());
-        btn1 = findViewById(R.id.btnAnswer1);
-        btn2 = findViewById(R.id.btnAnswer2);
-        btn3 = findViewById(R.id.btnAnswer3);
-        btn4 = findViewById(R.id.btnAnswer4);
+        //     0                 1                        2                  3,4,5,6
+        // Pregunta, cuenta atras inicio concurso, cuentra atras respuesta, preguntas
+        // Get intent [¿Cual en?, 3, 10, Los romanos, Las pitones, Los egipcios, Monty Python]
+
+        isFirstQuestion = getIntent().getBooleanExtra("isFirstQuestion", true);
+
+        if (isFirstQuestion){
+            respostes = getIntent().getStringArrayListExtra("arrayListRespostes");
+            counter = Integer.parseInt(respostes.get(2));
+            lengthAnswers = respostes.size();
+
+            countdown = findViewById(R.id.tvCountdown);
+            counter = Integer.parseInt(countdown.getText().toString());
+            btn1 = findViewById(R.id.btnAnswer1);
+            btn2 = findViewById(R.id.btnAnswer2);
+            btn3 = findViewById(R.id.btnAnswer3);
+            btn4 = findViewById(R.id.btnAnswer4);
+
+            // Change the button text
+            if (lengthAnswers == 5){
+                btn1.setText(respostes.get(3));
+                btn2.setText(respostes.get(4));
+                btn3.setVisibility(View.GONE);
+                btn4.setVisibility(View.GONE);
+            }else if(lengthAnswers == 6){
+                btn1.setText(respostes.get(3));
+                btn2.setText(respostes.get(4));
+                btn3.setText(respostes.get(5));
+                btn4.setVisibility(View.GONE);
+            }
+            else if(lengthAnswers == 7){
+                btn1.setText(respostes.get(3));
+                btn2.setText(respostes.get(4));
+                btn3.setText(respostes.get(5));
+                btn4.setText(respostes.get(6));
+            }
+        }
+
+
+
 
         new CountDownTimer(10000,1000) {
             @Override
